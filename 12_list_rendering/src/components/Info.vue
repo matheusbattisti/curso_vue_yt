2c2505e1fc3c0b5278f618367e522c5aab4f5ed8
Matheus Battisti <matheusbudkewicz@gmail.com>
2021-06-23 22:44:25 -0300

    Projeto

--- 12_list_rendering/src/components/Info.vue
+++ 12_list_rendering/src/components/Info.vue
@@ -2,12 +2,12 @@
   <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
   <p v-else>Estou em busca de novas oportunidades!</p>
   <p>Utilizo as seguintes tecnologias para back-end:</p>
-  <ul v-bind:key="index" v-for="(technology, index) in backend_technologies">
-    <li>{{ technology }}</li>
+  <ul>
+    <li v-bind:key="index" v-for="(technology, index) in backend_technologies">{{ technology }}</li>
   </ul>
     <p>Utilizo as seguintes tecnologias para front-end:</p>
-  <ul v-bind:key="index" v-for="(technology, index) in frontend_technologies">
-    <li>{{ technology }}</li>
+  <ul>
+    <li v-bind:key="index" v-for="(technology, index) in frontend_technologies">{{ technology }}</li>
   </ul>
   <div>
     <button @click="showEmail">Mostrar e-mail</button>
